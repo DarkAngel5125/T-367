49f85a15746c5c550fb8d20d82ff569fd05974e6
Kameko <Kameko@KURO>
2017-02-12 20:02:32 +0200

    Reduced the for loops

--- ConsoleApplication2/Source.cpp
+++ ConsoleApplication2/Source.cpp
@@ -5,40 +5,40 @@ int main()
 {
 	int a[30][30], n, m, i, j, k, p, q, v[60], d[60];
 	cin >> n >> m >> p >> q;
-	k = 0;
 	for (i = 1; i <= m; i++)
 		for (j = 1; j <= n; j++)
 			cin >> a[i][j];
 	for (i = 1; i <= m; i++)
 	{
-		if(i==q)
-		for (j = 1; j <= n; j++)
+		if (i == q)
 		{
+			k = 0;
+			for (j = 1; j <= n; j++)
+			{
 				k++;
 				v[k] = a[i][j];
+				cout << "V elements: " << v[k] << endl;
+			}
 		}
-	}
-	k = 0;
-	for (i = 1; i <= m; i++)
-	{
-		if (i == p)
-		for (j = 1; j <= n; j++)
+		else if (i == p)
 		{
-			k++;
-			d[k] = a[i][j];
+			k = 0;
+			for (j = 1; j <= n; j++)
+			{
+				k++;
+				d[k] = a[i][j];
+				cout << "D elements: " << d[k] << endl;
+			}
 		}
 	}
-    for (i = 1; i <= m; i++)
+	for (i = 1; i <= m; i++)
 	{
 		if (i == q)
 			for (j = 1; j <= n; j++)
 			{
 				a[i][j] = d[j];
 			}
-	}
-	for (i = 1; i <= m; i++)
-	{
-		if (i == p)
+		else if (i == p)
 			for (j = 1; j <= n; j++)
 			{
 				a[i][j] = v[j];
